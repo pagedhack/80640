21fa6a2b402dce9101dc8e4f5c54fb74681eb2c9
pagedhack <samu1@outlook.es>
2022-12-10 19:01:26 -0600

    evento

--- Practica05/agregar.js
+++ Practica05/agregar.js
@@ -25,26 +25,29 @@ function buscar(){
         console.log(dato);
     }*/
 
-    /*
-    for (let index = 0; index < listaTodo.length; index++) {
-        var arr = [].slice.call(listaTodo)
-        arr.forEach(element => { 
+    
+    // for (let index = 0; index < listaTodo.length; index++) {
+    //     var arr = [].slice.call(listaTodo)
+    //     arr.forEach(element => { 
            
-           if (arr == element) {
-            console.log("no se encuentra");
-           }else
-                console.log("se encuentra");
-        });
+    //        if (arr == element) {
+    //         console.log("no se encuentra");
+    //        }else
+    //             console.log("se encuentra");
+    //     });
         
-    }*/
+    // }
 
-    /*for (let index = 0; index < listaTodo.length; index++) {
+    for (let index = 0; index < listaTodo.length; index++) {
         const tarea = listaTodo[index];
         console.log(tarea);
         if(leer() == tarea.textContent){
                 console.log("existe");
             }
-    }*/
+        else{
+            console.log("no existe");
+        }
+    }
     
 
     /*if (document.getElementById((leer())) == null) {
@@ -52,19 +55,19 @@ function buscar(){
     }else 
     console.log("no existe");*/
 
-    for (const tarea in listaTodo) {
-        if (Object.hasOwnProperty.call(listaTodo, tarea)) {
-            //const tarea = listaTodo[tarea];
-            //if (leer() == tarea.childNodes[0]) {
-                //console.log(listaTodo[tarea]);
+    // for (const tarea in listaTodo) {
+    //     if (Object.hasOwnProperty.call(listaTodo, tarea)) {
+    //         //const tarea = listaTodo[tarea];
+    //         //if (leer() == tarea.childNodes[0]) {
+    //             //console.log(listaTodo[tarea]);
 
-                a = listaTodo[tarea].childNodes[0]
-                b = leer()
+    //             a = listaTodo[tarea].childNodes[0]
+    //             b = leer()
 
-                console.log("a ", typeof a.textContent, "b", typeof b )
+    //             console.log("a ", typeof a.textContent, "b", typeof b )
 
-                if (a == b)
-                console.log("ya existe");
+    //             if (a == b)
+    //             console.log("ya existe");
                     
                 
                     //if (leer() == listaTodo[tarea].childNodes[0]) {
@@ -75,6 +78,6 @@ function buscar(){
                     
             //}
             
-        }
-    }
+    //     }
+    // }
 }
